7aee9d6071ff796b309f2906b95a681ba4ab6fbf
13918346152 <13918346152>
2021-06-09 15:26:57 +0800

    immer使得不再需要扩展运算符来造新对象、新数组

--- src/ReactReduxTodo/reducer.ts
+++ src/ReactReduxTodo/reducer.ts
@@ -1,6 +1,6 @@
 import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "./actionTypes";
 import { TTodoAction } from "./actionCreators";
-// import produce from 'immer'
+import produce from 'immer'
 type THandler = (todoState: TTodoStore, action: TTodoAction) => TTodoStore;
 type THandlerMapper = { [key: string]: THandler };
 const initTodos: TTodo[] = [
@@ -28,8 +28,9 @@ const initState = {
 export const todosReducer = (todos: TTodo[] = initTodos, action: any) => {
   const handlerMapper = {
     [ADD_TODO]: (todos, action) => {
-      // produce(todos)
-      return [...todos, action.payload]
+      return produce(todos, draftTodos => {
+        draftTodos.push(action.payload)
+      })
     },
     [REMOVE_TODO]: (todos, action) => {
       return todos.filter(todo => todo.id !== action.payload)
